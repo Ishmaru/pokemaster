cd9aed6c88961a2cdb0e625cbd1323644ee2023a
Adrian Delpha <adrian.delpha@gmail.com>
2016-07-21 17:40:12 -0700

    heroku update

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var env = require('./environment');
 
 // Use different database URIs based on whether an env var exists.
-var dbUri = env.MLAB_URI ||
+var dbUri = process.env.MLAB_URI ||
             'mongodb://localhost/' + env.SAFE_TITLE;
 
 if (!env.MLAB_URI) {
